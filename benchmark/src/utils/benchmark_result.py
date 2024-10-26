836db4c97b5c48a08bc79da7151138b1b51ddb83
Mr IM Le Roux <24706825@su@panther.cs.sun.ac.za>
2024-10-26 12:44:12 +0200

    Updated with latest output format

--- benchmark/src/utils/benchmark_result.py
+++ benchmark/src/utils/benchmark_result.py
@@ -3,15 +3,18 @@ import re
 ELIMINATED_LABEL = "NUMBER OF TRANSITIONS ELIMINATED FROM FLATTENING:"
 MEMOISED_LABEL = "NUMBER OF STATES MEMOISED:"
 THREAD_LABEL = "TOTAL THREADS IN POOL"
+INITIALIZATION_LABEL = "Initialisation:"
 
 
 class BenchmarkResult():
     def __init__(self, stderr: str):
         self.parsed: dict[str, tuple[int, int]] = {}
-        pattern = re.compile(r"([A-Z]+): (\d+) \(FAILED: (\d+)\)")
+        pattern = re.compile(r"([A-Z_]+): (\d+) \(FAILED: (\d+)\)")
         self.eliminated = 0
         self.memoised = 0
         self.thread = 0
+
+        initializing = 0
         for line in stderr.strip().split("\n"):
             if line.startswith(MEMOISED_LABEL):
                 self.eliminated = int(line.split(': ')[1])
@@ -22,8 +25,20 @@ class BenchmarkResult():
             if line.startswith(THREAD_LABEL):
                 self.thread = int(line.split(': ')[1])
                 continue
+            if line.startswith(INITIALIZATION_LABEL):
+                initializing = 1
+                continue
+            if pattern.match(line) is not None:
+                initializing = 0
+
+            if initializing:
+                continue
 
             match = pattern.match(line)
+
+            if match is None:
+                print(line)
+
             assert match is not None
             total = int(match.group(2))
             failed = int(match.group(3))
@@ -31,15 +46,15 @@ class BenchmarkResult():
 
     @property
     def step(self) -> int:
-        return self.parsed["CHAR"][0] + self.parsed["PRED"][0]
+        return self.parsed["BRU_CHAR"][0] + self.parsed["BRU_PRED"][0]
 
     @property
     def memo_entry(self) -> int:
-        return self.parsed["MEMO"][0] - self.parsed["MEMO"][1]
+        return self.parsed["BRU_MEMO"][0] - self.parsed["BRU_MEMO"][1]
 
     @property
     def state(self) -> int:
-        return self.parsed["STATE"][0]
+        return self.parsed["BRU_STATE"][0]
 
     def to_dict(self):
         return {
